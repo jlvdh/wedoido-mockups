118f7cdb70701eb5dafa45202b842d3c5c8d4d21
Jorg van der Ham <jvdham@gmail.com>
2019-04-07 19:09:16 +0200

    Minor layout changes

--- public_html/assets/javascript/main.js
+++ public_html/assets/javascript/main.js
@@ -1,73 +1,83 @@
+/**
+ * smooth scroll init
+ */
+
+var scroll = new SmoothScroll('a[href*="#"]');
+
 var chapters = [];
 
 var Chapter = function(element) {
+  this.current = 0;
+  this.element = element;
+  this.slides = this.element.getElementsByClassName("vendor-container");
+};
+
+Chapter.prototype.setCurrent = function(change) {
+  if (change !== 1 || change !== 1) {
+    return;
+  }
+
+  // if (this.current + change > this.element.length || this.current + change)
+};
+Chapter.prototype.previous = function() {
+  this.hide(this.current);
+
+  if (this.current > 0) {
+    this.current--;
+  } else {
+    this.current = this.slides.length - 1;
+  }
+  this.show(this.current);
+};
+
+Chapter.prototype.next = function() {
+  this.hide(this.current);
+
+  if (this.current < this.slides.length - 1) {
+    this.current++;
+  } else {
     this.current = 0;
-    this.element = element;
-    this.slides = this.element.getElementsByClassName('vendor-container');
-}
-
-Chapter.prototype.setCurrent = function (change) {
-    if (change !== 1 || change !== 1) {
-        return
-    }
-
-    // if (this.current + change > this.element.length || this.current + change)
-}
-Chapter.prototype.previous = function () {
-    this.hide(this.current);
-
-    if (this.current > 0) {
-        this.current--
-    } else {
-        this.current = this.slides.length - 1;
-    }
-    this.show(this.current); 
-}
-
-Chapter.prototype.next = function () {
-
-    this.hide(this.current);
-
-    if (this.current < (this.slides.length - 1)) {
-        this.current++
-    } else {
-        this.current = 0;
-    }
-    this.show(this.current);
-}
+  }
+  this.show(this.current);
+};
 
 Chapter.prototype.hide = function(slideIndex) {
-    this.slides[slideIndex].style.display = "none";
-}
+  this.slides[slideIndex].style.display = "none";
+};
 
 Chapter.prototype.show = function(slideIndex) {
-    this.slides[slideIndex].style.display = "block";
-}
+  this.slides[slideIndex].style.display = "block";
+};
 
+Chapter.prototype.addListeners = function() {
+  var scrollElements = this.element.getElementsByClassName("scroll");
+  //left
+
+  // event listeners bind this to click event
+  var _this = this;
+
+  scrollElements[0].addEventListener("click", function(e) {
+    _this.previous();
+  });
+
+  //right
+  scrollElements[1].addEventListener("click", function(e) {
+      console.log(e)
+    _this.next();
+  });
+};
 
 window.onload = function() {
+  var chapterElements = document.getElementsByClassName("chapter");
 
-    var chapterElements = document.getElementsByClassName('chapter');
+  for (var i = 0; i < chapterElements.length; i++) {
+    var chapter = new Chapter(chapterElements[i]);
+    chapters.push(chapter);
 
-    for (var i=0; i < chapterElements.length; i++) {
-        var chapter = new Chapter(chapterElements[i]);
-        chapters.push(chapter)
-        var scrollElements = chapters[i].element.getElementsByClassName('scroll')
+    chapter.addListeners();
+    chapter.show(0);
+  }
 
-        //left
-        scrollElements[0].addEventListener('click', function() {
-            //TODO click events not working correctly: address from clickevent
-            chapter.previous();
-        })
-
-        //right
-        scrollElements[1].addEventListener('click', function() {
-            chapter.next();
-        });
-        chapter.show(0);
-    }
-
-
-    // for each chapter add listener with right index
-    // add eventlisteners
-}
+  // for each chapter add listener with right index
+  // add eventlisteners
+};
